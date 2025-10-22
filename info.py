abbca7e6368be82f35b37132a571d9b2eb040965
owajbhai <owajshaikhrcm@gmail.com>
2025-10-22 13:41:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -65,7 +65,7 @@ LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '-1002314687215'))
 LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-1002314687215'))
 VERIFY_IMG = environ.get("VERIFY_IMG", "https://telegra.ph/file/9ecc5d6e4df5b83424896.jpg")
 
-TUTORIAL = environ.get("TUTORIAL", "https://t.me/")
+TUTORIAL = environ.get("TUTORIAL", "https://t.me/payment_prime/9")
 TUTORIAL_2 = environ.get("TUTORIAL_2", "https://t.me/")
 TUTORIAL_3 = environ.get("TUTORIAL_3", "https://t.me/")
 
