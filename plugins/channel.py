a3468aa2d5f25bae6dd3c1a3962ebd398e35e8a9
HiiFyi <167199917+HiiFyi@users.noreply.github.com>
2025-10-23 07:24:23 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -88,10 +88,10 @@ async def send_movie_update(bot, file_name, caption):
         if poster:
           #  photo_file = io.BytesIO(poster)
           #  photo_file.name = await generate_random_filename()
-            await bot.send_photo(chat_id=MOVIE_UPDATE_CHANNEL, caption=full_caption, reply_markup=InlineKeyboardMarkup(buttons))    
+            await bot.send_message(chat_id=MOVIE_UPDATE_CHANNEL, caption=full_caption, reply_markup=InlineKeyboardMarkup(buttons))    
         else:
            # image_url = "https://te.legra.ph/file/88d845b4f8a024a71465d.jpg"   
-            await bot.send_photo(chat_id=MOVIE_UPDATE_CHANNEL, caption=full_caption, reply_markup=InlineKeyboardMarkup(buttons))                
+            await bot.send_message(chat_id=MOVIE_UPDATE_CHANNEL, caption=full_caption, reply_markup=InlineKeyboardMarkup(buttons))                
     except Exception as e:
         print(f"Error in send_movie_update: {e}")
 
