45af4498e025743e0e2b91714a40ff5f79043d51
owajbhai <owajshaikhrcm@gmail.com>
2025-11-02 23:33:12 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -23,7 +23,7 @@ SILENTX_UPDATE_CAPTION = """<i><b><blockquote>📫 𝖭𝖤𝖶 𝖥𝖨𝖫𝖤
 ✅ FILE NAME - {} #{}
 🎧 𝖠𝗎𝖽𝗂𝗈 - {}
 
-<b><blockquote>Uploaded By -@SpyRadio55</blockquote></b>
+<b><blockquote>Uploaded By -@Prime_Movie_Request_bot</blockquote></b>
 """
 
 notified_movies = set()
